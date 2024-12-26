12f973d3b27790539933f8b1178493522832c482
xtrinch <mojca.rojko@povio.com>
2024-12-26 16:35:58 +0100

    .

---
+++ src/routes/_auth.profile.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_auth/profile")({
+  component: ProfileComponent,
+});
+
+function ProfileComponent() {
+  const { username } = Route.useRouteContext();
+
+  return (
+    <div className="p-2 space-y-2">
+      <div>
+        Username:<strong>{username}</strong>
+      </div>
+    </div>
+  );
+}
